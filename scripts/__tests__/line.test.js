940f91e6e150c4be6a9891a52c13a28c65102791
Océane Patiny <oceane@patiny.com>
2023-05-04 11:16:20 +0200

    fix: debug line

---
+++ scripts/__tests__/line.test.js
@@ -0,0 +1,14 @@
+var LM = require("../../src/index");
+var Matrix = LM.Matrix;
+var math = Matrix.algebra;
+
+var line = require("../line.js");
+
+var t = math.matrix([[0], [1], [2], [3], [4], [5]]);
+var p = math.matrix([[2], [1]]);
+console.log(t);
+
+console.log(p);
+var y = line(t, p);
+console.log(y);
+// should be [1,3,5,7,9,11]
---
+++ scripts/line.js
@@ -0,0 +1,14 @@
+var LM = require("../src/index");
+var Matrix = LM.Matrix;
+var math = Matrix.algebra;
+
+function line(t, p) {
+  var result = math.matrix(t.rows, 1);
+  for (var i = 0; i < t.rows; i++) {
+    console.log("current row: " + t[i][0]);
+    result[i][0] = p[0][0] * t[i][0] + p[1][0];
+  }
+  return result;
+}
+
+module.exports = line;
